f4d9e2287f0a6ca4fb031114d833407a7c8731ff
sxjscience <sxjscience001@gmail.com>
2019-06-25 18:29:02 -0700

    fix

--- topi/python/topi/transform.py
+++ topi/python/topi/transform.py
@@ -488,6 +488,6 @@ def sequence_mask(data, seq_length=None, use_seq_length=False, value=0, axis=0, 
     def _compute(*indices):
         tid = indices[axis]
         bid = indices[1 - axis]
-        ret = value if tid >= seq_length(bid) else data(*indices)
+        ret = tvm.if_then_else(tid >= seq_length(bid), value, data(*indices))
         return ret
     return tvm.compute(data.shape, _compute, name=name)
