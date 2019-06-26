b18e573126f0170caffa9dd77376e66bc93a6c79
sxjscience <sxjscience001@gmail.com>
2019-06-26 16:30:57 -0700

    fix bug

--- topi/src/topi.cc
+++ topi/src/topi.cc
@@ -339,7 +339,7 @@ TVM_REGISTER_GLOBAL("topi.take")
 
 TVM_REGISTER_GLOBAL("topi.sequence_mask")
 .set_body([](TVMArgs args, TVMRetValue *rv) {
-  *rv = sequence_mask(args[0], args[1], axis, mode);
+  *rv = sequence_mask(args[0], args[1], args[2], args[3]);
 });
 
 
