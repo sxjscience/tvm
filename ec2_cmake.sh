a439c8cc0428d4323426bd416c2963c828269fd5
Xingjian Shi <xshiab@connect.ust.hk>
2019-08-13 18:14:58 -0700

    Update ec2_cmake.sh

--- ec2_cmake.sh
+++ ec2_cmake.sh
@@ -2,7 +2,7 @@ if [ ! -d "./build" ]; then
     mkdir build
 fi
 cd build
-rm CMakeCache.txt
+rm -r *
 cmake -DUSE_LLVM=/usr/bin/llvm-config-6.0 -DUSE_CUDA=ON -DUSE_SORT=ON ..
 make -j `nproc --all`
 cd ..
